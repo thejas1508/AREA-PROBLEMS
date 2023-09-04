86099b864c5821fdda115a5d4787765c5814baf6
THEJAS KUMAR.B <84064959+thejas1508@users.noreply.github.com>
2023-09-04 11:12:13 +0530

    Circle.py

--- Circle.py
+++ Circle.py
@@ -1,6 +1,4 @@
 Radius = int(input(“Enter the radius :”) )
-
-              Area = 3.14*radius**2
-   
-              print(“Area of the circle is :”,Area)
+Area = 3.14*radius**2
+print(“Area of the circle is :”,Area)
 
